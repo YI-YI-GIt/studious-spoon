97d84e342684d4634d040903fb372f8dccd6feb6
iwaby <iwaby@192.168.0.46>
2025-08-02 01:58:04 +0900

    Java導入

---
+++ HelloWorld_IwataYuki/src/sample/SelfIntroduction.java
@@ -0,0 +1,23 @@
+package sample;
+
+public class SelfIntroduction {
+
+	public static void main(String[] args) {
+// 氏名
+String name = "岩田 夕輝";
+// 年齢
+int age = 24;
+// 趣味
+String hobby = "K-pop鑑賞";
+
+/*
+* 自己紹介文を出力する処理
+*/
+
+System.out.println("私の名前は" + name + "です。");
+System.out.println("年齢は" + age + "歳です。");
+System.out.println("趣味は" + hobby + "です。");
+
+	}
+
+}
