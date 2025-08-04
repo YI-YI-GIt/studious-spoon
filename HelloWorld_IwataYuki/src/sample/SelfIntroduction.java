f1c1bcb8b5b166af121927738732b12d6285d9ff
iwaby <iwaby@192.168.0.46>
2025-08-04 09:33:46 +0900

    curriculum_A

--- HelloWorld_IwataYuki/src/sample/SelfIntroduction.java
+++ HelloWorld_IwataYuki/src/sample/SelfIntroduction.java
@@ -3,21 +3,38 @@ package sample;
 public class SelfIntroduction {
 
 	public static void main(String[] args) {
-// 氏名
-String name = "岩田 夕輝";
-// 年齢
-int age = 24;
-// 趣味
-String hobby = "K-pop鑑賞";
+	// 氏名
+		String name = "岩田 夕輝";
+		// 年齢
+		int age = 24;
+		// 趣味
+		String hobby = "K-POP鑑賞";
 
-/*
-* 自己紹介文を出力する処理
-*/
 
-System.out.println("私の名前は" + name + "です。");
-System.out.println("年齢は" + age + "歳です。");
-System.out.println("趣味は" + hobby + "です。");
+		/*
+		* 自己紹介文を出力する処理
+		*/
 
+		System.out.println("私の名前は" + name + "です。");
+		System.out.println("年齢は" + age + "歳です。");
+		System.out.println("趣味は" + hobby + "です。");
+		
+
+		method1();
+		
+        method2();
+	}
+
+	
+	
+	public static String method1() {
+		return "岩田";
+	
+	}
+	
+	public static String method2() {
+		return "夕輝";
+		
 	}
 
 }
