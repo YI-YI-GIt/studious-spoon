27ab67c7df4146130096f76b7d089d91fabd6dbd
iwaby <iwaby@192.168.0.39>
2025-09-01 22:44:11 +0900

    Self_Introduce

---
+++ HelloWorld_IwataYuki/src/selfIntroduseProgram/Main.java
@@ -0,0 +1,18 @@
+package selfIntroduseProgram;
+
+class Main {
+	public static void main(String[] args) {
+		Person person1 = new Person("鈴木太郎", 20, 1.7, 60);// 問５		
+		
+	    System.out.println(person1.getName());
+	    System.out.println(person1.getAge());
+	    System.out.println(person1.getHeight());
+	    System.out.println();
+	    
+	    person1.print();
+	    System.out.println();
+	    
+	    person1.count();
+	  }
+	
+	}
---
+++ HelloWorld_IwataYuki/src/selfIntroduseProgram/Person.java
@@ -0,0 +1,48 @@
+package selfIntroduseProgram;
+
+class Person{
+	// 問１ インスタンスフィールドを定義
+	private String name;
+	private int age;
+	private double height;
+	private double weight;
+	private int count = 0; // 問１０
+	
+	// 問２,３ コンストラクタを定義しインスタンスフィールドに値をセット
+	public Person(String name, int age, double height, double weight) {// 問４
+		  this.name = name;
+		  this.age = age;
+		  this.height = height;
+		  this.weight = weight;
+		  count++; // 問１０
+		  
+	}
+	public String getName() {
+		return name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public double getHeight() {
+		return height;
+	}
+	public double getWeight() {
+		return weight;
+	}
+	
+	// 問６,７インスタンスメソッド bmi
+	public double bmi() {
+		double rawBmi = weight / (height * height);
+		return Math.floor(rawBmi * 10)/10; // 小数点第２以降切り捨て
+	}
+	// 問８,９
+	public void print() {
+		System.out.println("名前は" + this.name + "です");
+		System.out.println("年は" + this.age+ "です");
+		System.out.println("BMIは" + this.bmi() + "です");
+	}
+	// 問１０
+	public void count() {
+		System.out.println("合計" + count + "人です");
+	}
+}
